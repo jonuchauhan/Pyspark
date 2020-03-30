37b2b50dfd39d79d48bb0220abf53e51dc8f812c
Chauhan Jonu EXT <ext-jonu.chauhan@elisa.fi>
2020-03-30 23:57:17 +0300

    WordCount using pyspark

--- WordCountFlatmap.py
+++ WordCountFlatmap.py
@@ -2,8 +2,13 @@ from pyspark import SparkContext, SparkConf
 
 conf = SparkConf().setMaster("local").setAppName("WordCountFlatmap")
 sc = SparkContext(conf=conf)
-input = sc.textFile("C:\spark\README.MD")
+input = sc.textFile("C:\spark\TEXT.txt")
 words = input.flatMap(lambda x: x.split())
 wordcount = words.countByValue()
 print(wordcount)
+
+for word, count in wordcount.items():
+    print(word, count)
+
+
 
