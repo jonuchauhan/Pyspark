9ae1eae399f787a74a88f76d430fdfdfef629657
Chauhan Jonu EXT <ext-jonu.chauhan@elisa.fi>
2020-04-04 02:33:52 +0300

    word count in sorted order

--- WordCountFlatmap.py
+++ WordCountFlatmap.py
@@ -3,12 +3,8 @@ from pyspark import SparkContext, SparkConf
 conf = SparkConf().setMaster("local").setAppName("WordCountFlatmap")
 sc = SparkContext(conf=conf)
 input = sc.textFile("C:\spark\TEXT.txt")
-words = input.flatMap(lambda x: x.split())
-wordcount = words.countByValue()
-print(wordcount)
+words = input.flatMap(lambda x: x.split()).map(lambda x: (x, 1))
+wordcount = words.reduceByKey(lambda x, y: (x+y)).map(lambda x: (x[1], x[0])).sortByKey(ascending= False)
 
-for word, count in wordcount.items():
-    print(word, count)
-
-
-
+for count, word in wordcount.collect():
+    print(word, '=', count)
