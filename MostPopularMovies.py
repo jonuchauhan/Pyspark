5ccb10e010c85fb171856a9a14fc694287a4e5d6
Chauhan Jonu EXT <ext-jonu.chauhan@elisa.fi>
2020-04-05 00:11:03 +0300

    Modified code removed extra loop

--- MostPopularMovies.py
+++ MostPopularMovies.py
@@ -15,11 +15,9 @@ input = sc.textFile("C:/GitHub/PYSPARK/Pyspark/Data/ratings.csv")
 MaximumMovies = input.map(parsedinput).map(lambda x: (x[0], 1)).reduceByKey(lambda x, y: (x + y)).map(
     lambda x: (x[1], x[0])).sortByKey(
     ascending=False)
-MaximumratingByMovie = input.map(parsedinput).reduceByKey(lambda x, y: (x + y)).map(lambda x: (x[1], x[0])).\
-    sortByKey(ascending=False)
+
 
 for a in MaximumMovies.collect():
     print("MOVIEID", a[1], "=", a[0])
 
-for b in MaximumratingByMovie.collect():
-    print("MOVIEID", a[1], "=", a[0])
+
